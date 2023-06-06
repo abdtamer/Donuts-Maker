96c17609e2fdde3bf4cec0136dedc698ede499b8
Tamer AbdelMageed <133507952+abdtamer@users.noreply.github.com>
2023-06-06 19:32:41 -0400

    Update donutclicker.js

--- donutclicker.js
+++ donutclicker.js
@@ -1,100 +1,92 @@
-let nav = document.querySelector("nav");
-
-let collapsibleNavBar = document.getElementsByClassName("collapsibleNavBar");
-let i;
-
-for (i=0; i< collapsibleNavBar.length; i++) {
-    collapsibleNavBar[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if(content.style.display === "block") {
-            content.style.display = "none";
-            nav.style.marginBottom = "0px";
-        } else {
-            content.style.display = "block";
-            nav.style.marginBottom = "65px";
-        }
-    });
+const donut = {
+  "count": 95
 }
 
-let donutButton = document.images["donutButton"];
-let donutButtonBeep = new Audio();
-donutButtonBeep.src = 'src/DonutButtonSound.mp3';
-let purchaseAutoClicker = document.querySelector("#purchase-auto-clicker");
-let autoClickerPriceSpan = document.querySelector("#auto-clicker-price");
-let autoClickerSpan = document.querySelector("#auto-clickers-total");
-let purchaseClickMultipliers = document.querySelector("#purchase-click-multiplier");
-let clickMultiplierPriceSpan = document.querySelector("#click-multiplier-price");
-let clickMultiplierSpan = document.querySelector("#click-multiplier");
-let resetButton = document.querySelector("#reset-game-button");
+const autoClicker = {
+  "count": 0,
+  "cost": 100,
 
-donutButton.addEventListener("click", addDonut);
-donutButton.addEventListener("click", playAudio);
-purchaseAutoClicker.addEventListener("click",purchaseAutoClickerCount);
-purchaseClickMultipliers.addEventListener("click", purchaseClickMultiplier);
-resetButton.addEventListener("click", resetGame);
-
-let donutNumber = document.querySelector(".donut-count");
-let donutCount = 0;
-let autoClickerPrice = 100;
-let autoClickerCount = 0;
-let clickMultiplierPrice = 10;
-let clickMultiplierCount = 0;
-
-function addDonut() {
-    donutCount += Math.pow(1.2, clickMultiplierCount);
-    donutNumber.innerText = numberWithCommas(Math.round(donutCount));
 }
 
-function playAudio() {
-    donutButtonBeep.play();
+const donutMultiplier = {
+  "count": 0,
+  "cost": 10,
+  "clickValue": 0
 }
 
-function retrieveDonutCount() {
-    return donutCount;
+const autoClickerButton = document.querySelector(".auto-clickers__buy");
+const donutMultButton = document.querySelector(".donut-multipliers__buy");
+
+autoClickerButton.disabled = true;
+donutMultButton.disabled = true;
+
+const createDonut = () => {
+  if (donutMultiplier.count > 0) {
+      donut.count += Math.pow(1.2, donutMultiplier.count);
+      donutMultiplier.clickValue = Math.pow(1.2, donutMultiplier.count);
+      document.querySelector(".donut-multipliers__value").innerHTML = donutMultiplier.clickValue;
+      document.querySelector(".donuts__created").innerHTML = donut.count;
+  } else {
+      donut.count += 1;
+      document.querySelector(".donuts__created").innerHTML = donut.count;
+  }
+  donut.count >= autoClicker.cost ? autoClickerButton.disabled = false : autoClickerButton.disabled = true;
+  donut.count >= donutMultiplier.cost ? donutMultButton.disabled = false : donutMultButton.disabled = true;
 }
 
-function purchaseAutoClickerCount() {
-    if (donutCount >= autoClickerPrice) {
-        donutCount -= autoClickerPrice;
-        autoClickerCount += 1;
-        autoClickerPrice =Math.round(autoClickerPrice * 1.10);
-        donutNumber.innerText = numberWithCommas(Math.round(donutCount));
-        autoClickerPriceSpan.innerText =numberWithCommas (autoClickerPrice);
-        autoClickerSpan.innerText = numberWithCommas(autoClickerCount);
-        if (autoClickerCount <= 1) {
-            activateAutoClickers();
-    }  
-}
+
+const purchaseAutoClicker = (donut, autoClicker) => {
+  if (donut.count < autoClicker.cost) {
+      return;
+  } else {
+      document.querySelector(".donuts__created").innerHTML = donut.count - autoClicker.cost;
+      donut.count -= autoClicker.cost;
+      document.querySelector(".auto-clickers__created").innerHTML = autoClicker.count += 1;
+      
+      autoClicker.cost += (autoClicker.cost * .10);
+      document.querySelector(".auto-clickers__cost").innerHTML = autoClicker.cost;
+      setInterval(createDonut, 1000);
+      
+  }
+
 }
 
-function retrieveAutoClickerCount() {
-    return autoClickerCount;
+const purchaseDonutMultiplier = () => {
+
+  if(donut.count < donutMultiplier.cost) {
+      return;
+  } else {
+
+      document.querySelector(".donuts__created").innerHTML = donut.count - donutMultiplier.cost;
+      donut.count -= donutMultiplier.cost;
+      document.querySelector(".donut-multipliers__created").innerHTML = donutMultiplier.count += 1;
+      donutMultiplier.cost += (donutMultiplier.cost * .10);
+      document.querySelector(".donut-multipliers__cost").innerHTML = donutMultiplier.cost;
+  }
 }
 
-function activateAutoClickers() {
-    setInterval(function(){
-    donutCount += autoClickerCount * Math.pow(1.2, clickMultiplierCount);
-    donutNumber.innerText = numberWithCommas(Math.round(donutCount));
-    }, 1000);
-    
+const activateAutoClickersEvent = () => {
+  autoClicker.count += donutMultiplier.count;
+  donut.count += autoClicker.count;
 }
 
-function purchaseClickMultiplier() {
-    if (donutCount >= clickMultiplierPrice) {
-        donutCount -= clickMultiplierPrice;
-        clickMultiplierCount += 1;
-        clickMultiplierPrice = Math.round(clickMultiplierPrice * 1.1);
-        donutNumber.innerText = numberWithCommas(Math.round(donutCount));
-        clickMultiplierPriceSpan.innerText = numberWithCommas(clickMultiplierPrice);
-        clickMultiplierSpan.innerText = numberWithCommas (clickMultiplierCount);
-    }
-}
+const donutClick = document.querySelector(".donuts__maker");
+donutClick.addEventListener("click", () => {
+  event.preventDefault();
+  createDonut();
+})
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-}
+const autoClickerClick = document.querySelector(".auto-clickers__buy");
+autoClickerClick.addEventListener("click", () => {
+  event.preventDefault();
+  purchaseAutoClicker(donut,autoClicker);
+})
 
-function resetGame(){
-    location.reload();
-} 
+const donutMultiplierClick = document.querySelector(".donut-multipliers__buy");
+  donutMultiplierClick.addEventListener("click", () => {
+  event.preventDefault();
+  purchaseDonutMultiplier();
+})
+
+
+
